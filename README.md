3b7dc56e4e50f829401ebdb7b932a932d8acfd64
Laiba Choudhry <laiba@hotmail.nl>
2024-08-01 00:00:52 +0200

    draft 1

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # tommymyway_final
 This is the final prototype for my graduation project. The prototypes name is Tommy my way. 
 
-This prototype works in combination with the BLE devices. 
+This prototype works in combination with the BLE devices and also a desktop and smartphones. 
 
 See here the whole flow
 https://miro.com/app/board/uXjVK2aO2Tg=/ 
+
+See here the whole design 
+https://www.figma.com/design/cVMvpMOj7ZY5G1UVvOchF1/afstuderen-prototype?node-id=0-1&t=cerNshNfY0RNC257-0 
+
 
 
--- script/index.js
+++ script/index.js
@@ -1,3 +1,14 @@
+document.getElementById('openSmartThingsBtn').addEventListener('click', function() {
+    
+    const smartThingsUrl = 'intent://#Intent;scheme=oneconnect;package=com.samsung.android.oneconnect;end';
 
-alert("JavaScript is working!");
-console.log("JavaScript is successfully running.");
+    // Create an invisible iframe to attempt to open the SmartThings app
+    const iframe = document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '0';
+    iframe.style.height = '0';
+    iframe.style.display = 'none';
+    iframe.src = smartThingsUrl;
+
+    document.body.appendChild(iframe);
+});
